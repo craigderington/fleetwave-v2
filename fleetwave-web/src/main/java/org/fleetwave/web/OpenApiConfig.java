6ffe643410bf214b5d9e1d58c65383d35b0670b2
Craig Derington <craig@craigderington.dev>
2025-10-26 15:21:51 -0400

    fix timezone build error

--- fleetwave-web/src/main/java/org/fleetwave/web/OpenApiConfig.java
+++ fleetwave-web/src/main/java/org/fleetwave/web/OpenApiConfig.java
@@ -1,1 +1,8 @@
-package org.fleetwave.web; import io.swagger.v3.oas.annotations.*; import org.springframework.context.annotation.Configuration; @Configuration @OpenAPIDefinition(info=@Info(title="FleetWave API", version="v1")) public class OpenApiConfig{}
+package org.fleetwave.web; 
+import io.swagger.v3.oas.annotations.*; 
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration; 
+
+
+@Configuration @OpenAPIDefinition(info=@Info(title="FleetWave API", version="v1")) 
+public class OpenApiConfig{}
